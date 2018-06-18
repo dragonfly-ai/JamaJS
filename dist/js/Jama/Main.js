6d24a590ea74b16d99c96d90b936f7edea34af78
c <c@dai2>
2018-06-18 09:11:33 -0600

    Added manually generated Main.js as the CommonJS entry point for webpack npm compatibility.

---
+++ dist/js/Jama/Main.js
@@ -0,0 +1,18 @@
+/*
+  This manually generated file handles CommonJS exports for node and webpack.
+  Do not delete.
+*/
+
+var mat = require("./Matrix.js");
+var svd = require("./SingularValueDecomposition.js");
+var chol = require("./CholeskyDecomposition.js");
+var eig = require("./EigenvalueDecomposition.js");
+var lud = require("./LUDecomposition.js");
+var qr = require("./QRDecomposition.js");
+
+exports.Matrix = mat.Matrix;
+exports.SingularValueDecomposition = svd.SingularValueDecomposition;
+exports.CholeskyDecomposition = chol.CholeskyDecomposition;
+exports.EigenvalueDecomposition = eig.EigenvalueDecomposition;
+exports.LUDecomposition = lud.LUDecomposition;
+exports.QRDecomposition = qr.QRDecomposition;
